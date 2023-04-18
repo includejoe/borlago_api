91b2fa7ba3f3b77eb3d398749c2e00bd7ef2b932
joel ayertey <kvnqjoe@gmail.com>
2023-04-18 19:37:57 +0000

    mc

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin,
+)
+
 
 # Create your models here.
