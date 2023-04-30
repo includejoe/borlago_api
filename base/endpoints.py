ec38c471afe239de5caa6441577cf71843f0769d
joel ayertey <kvnqjoe@gmail.com>
2023-04-30 20:44:34 +0000

    + collector app

--- base/endpoints.py
+++ base/endpoints.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 urlpatterns = [
     path("auth/", include("authentication.urls")),
     path("administrator/", include("administrator.urls")),
+    path("collector/", include("collector.urls")),
     path("user/", include("user.urls")),
     path("wcr/", include("waste.urls")),
 ]
