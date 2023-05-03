32ba41411e692a77e4b71b35e16ec874f1cb3ad2
joel ayertey <kvnqjoe@gmail.com>
2023-05-03 11:43:18 +0000

    check usertype on login

--- authentication/views.py
+++ authentication/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
 
 from . import serializers
+from user.models import User
 
 
 # Create your views here.
@@ -27,8 +28,31 @@ class LoginAPIView(GenericAPIView):
     serializer_class = serializers.LoginSerializer
 
     def post(self, request):
-        user = request.data
-        serializer = self.serializer_class(data=user)
+        user_data = request.data
+        user_type = request.data.get("user_type", None)
+        email = request.data["email"]
+        user = User.objects.get(email=email)
+
+        if user_type == 1:
+            if user.user_type != 1:
+                return Response(
+                    {"detail": "This user is not an administrator"},
+                    status=status.HTTP_403_FORBIDDEN,
+                )
+        elif user_type == 2:
+            if user.user_type != 2:
+                return Response(
+                    {"detail": "This user is not a normal user"},
+                    status=status.HTTP_403_FORBIDDEN,
+                )
+        elif user_type == 3:
+            if user.user_type != 3:
+                return Response(
+                    {"detail": "This user is not a collector"},
+                    status=status.HTTP_403_FORBIDDEN,
+                )
+
+        serializer = self.serializer_class(data=user_data)
 
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
